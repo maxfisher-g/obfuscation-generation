e653cbd19cc0ca9156db3231e0c82fadbf703c16
Max Fisher <maxfisher@google.com>
2023-07-05 12:17:41 +1000

    improve extract script
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- npm-download/extract_all.sh
+++ npm-download/extract_all.sh
@@ -1,7 +1,11 @@
+#! /bin/bash
+
+DEST_DIR=../non-obfuscated-files
+
 for f in *; do
-	echo $f
-	export EXTRACT_DIR=${f%%.tgz}
-	mkdir $EXTRACT_DIR && tar -xf $f -C $EXTRACT_DIR && find $EXTRACT_DIR -name "*.js" | ./flatten_directory_tree.py ../non-obfuscated-files && \rm -rf $EXTRACT_DIR
+	echo "$f"
+	export EXTRACT_DIR="${f%%.tgz}"
+	mkdir "$EXTRACT_DIR" && tar -xf "$f" -C "$EXTRACT_DIR" && find "$EXTRACT_DIR" -name "*.js" | ./flatten_directory_tree.py "$DEST_DIR" && \rm -rf "$EXTRACT_DIR"
 	unset EXTRACT_DIR
 done
 
