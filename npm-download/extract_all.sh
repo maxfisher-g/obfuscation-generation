eeab2f834b3cace97e56dc2339643be703b7c8e7
Max Fisher <maxfisher@google.com>
2023-07-20 02:02:03 +1000

    rename flatten_directory_tree.py -> flatten_filetree.py and improve comments in extract_all.sh
    Signed-off-by: Max Fisher <maxfisher@google.com>

--- npm-download/extract_all.sh
+++ npm-download/extract_all.sh
@@ -11,9 +11,14 @@ function extract {
 	echo "$1"
 	export EXTRACT_DIR=$(basename "${1%%.tgz}")
 	mkdir "$EXTRACT_DIR"
+
+	# some NPM archives have directories without the exec permission set, so
+	# --delay-directory-restore plus the following find command is needed
 	tar -xf "$1" -C "$EXTRACT_DIR" --delay-directory-restore
 	find "$EXTRACT_DIR" -type d -exec chmod +x '{}' ';'
-	find "$EXTRACT_DIR" -type f -name "*.js" | ./flatten_directory_tree.py -d "$DEST_DIR" && \rm -rf "$EXTRACT_DIR"
+
+	# move/rename EXTRACT_DIR/a/b/c.js to DEST_DIR/a_b_c.js
+	find "$EXTRACT_DIR" -type f -name "*.js" | ./flatten_filetree.py -d "$DEST_DIR" && \rm -rf "$EXTRACT_DIR"
 	unset EXTRACT_DIR
 }
 
